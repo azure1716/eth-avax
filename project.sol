71750c2ee4d46def901544bfdbec7bce3c1bdb2b
azure1716 <harshksharma1716@gmail.com>
2024-07-27 13:28:09 +0530

    project.sol

---
+++ project.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Error_handling {
+    function mul(uint i) public pure returns(uint) {
+        require(i > 0, "Input must be greater than 0");
+        return i*2;
+    }
+
+    function square(uint i) public pure returns(uint) {
+        if (i <= 0) {
+            revert("Input must be greater than 0");
+        }
+        else{
+            return i*i;
+        }
+    }
+
+    uint public num = 0;
+
+    function add() public view returns(uint){
+        assert(num >= 0);
+        return num + 2;
+    }
+}
